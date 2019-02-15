cd84895eea68cf32e636352ef21d0b780af97b7b
lwood42 <40302819+lwood42@users.noreply.github.com>
2019-02-15 11:16:11 +0000

    Update service.php

--- service.php
+++ service.php
@@ -10,7 +10,7 @@ if (mysqli_connect_errno())
 }
  
 // This SQL statement selects ALL from the table 'Employees'
-$sql = "SELECT * FROM Employees";
+$sql = "SELECT * FROM employeeTest";
  
 // Check if there are results
 if ($result = mysqli_query($con, $sql))
